8e74390efe327628277ccc31fb19669ef612bba0
xxshutong <xxshutong@gmail.com>
2013-07-07 22:35:16 +0800

    Fix up static admin style

--- erp/settings.py
+++ erp/settings.py
@@ -13,6 +13,8 @@ ADMINS = (
 # ('Your Name', 'your_email@example.com'),
 )
 
+ADMIN_PREFIX = 'admin'
+
 MANAGERS = ADMINS
 
 DB_USERNAME = os.getenv('DB_USERNAME', 'wjzpw')
@@ -60,13 +62,13 @@ MEDIA_ROOT = PROJECT_DIR + WEBVERSE_DIR
 # URL that handles the media served from MEDIA_ROOT. Make sure to use a
 # trailing slash.
 # Examples: "http://example.com/media/", "http://media.example.com/"
-MEDIA_URL = ''
+MEDIA_URL = os.getenv('HOME_URL', 'http://localhost:8000/')
 
 # Absolute path to the directory static files should be collected to.
 # Don't put anything in this directory yourself; store your static files
 # in apps' "static/" subdirectories and in STATICFILES_DIRS.
 # Example: "/var/www/example.com/static/"
-STATIC_ROOT = ''
+STATIC_ROOT = MEDIA_URL + 'static/'
 
 # URL prefix for static files.
 # Example: "http://example.com/static/", "http://static.example.com/"
@@ -131,6 +133,7 @@ INSTALLED_APPS = (
     'django.contrib.sites',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'django.contrib.admin',
     'erp.core',
     'south',
     # Uncomment the next line to enable the admin:
